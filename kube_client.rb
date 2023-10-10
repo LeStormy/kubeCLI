9fef743e78e0586f9a9a53bf1c9571796ec3d101
stormy <raphael.beltrand@gmail.com>
2023-10-10 09:12:23 +0200

    add command

--- kube_client.rb
+++ kube_client.rb
@@ -3,21 +3,35 @@
 require './kube_commands.rb'
 
 class KubeClient
+  DOCKER_HUB_UNAME = 'lestormy'
+  PROJECT_PATH = '/Users/stormy/Work/mooveo-backend'
+  CONFIG_PATH = 'config/kube/infrastructure'
+  APP = 'klara'
+  VERSION = '1.0'
+
   COMMANDS = {
     'migrate' => 'Apply a migration manifest',
     'deploy' => 'Apply a deployment manifest',
-    'scale' => 'Scale a deployment',
+    'delete' => 'Delete a kubectl object [args: manifest name]'
+    'scale' => 'Scale a deployment [arg: manifest name]',
+    'build-setup' => 'Build a new docker image and setup a new namespace with an app stack',
     'setup' => 'Setup a new namespace with an app stack',
     'console' => 'Connect to a shell on a pod and run bin/rails c',
-    'exec' => 'Execute a command inside a pod'
+    'add-job' => 'Apply a cron job manifest [arg: manifest name]',
+    'delete-job' => 'Delete a cron job [arg: manifest name]',
+    'exec' => 'Execute a command inside a pod [arg: command to run]'
   }
 
   COMMANDS_MAPPING = {
-    'migrate' => -> (namespac, args) { migrate(namespace) },
+    'migrate' => -> (namespace, args) { migrate(namespace) },
     'deploy' => -> (namespace, args) { deploy(namespace) },
-    'scale' => -> (namespace, args) { scale(namespace) },
+    'delete' => -> (namespace, args) { delete(namespace, args) },
+    'scale' => -> (namespace, args) { scale(namespace, args) },
+    'build-setup' => -> (namespace, args) { build_setup(namespace) },
     'setup' => -> (namespace, args) { setup(namespace) },
     'console' => -> (namespace, args) { console(namespace) },
+    'add-job' => -> (namespace, args) { add_job(namespace, args) },
+    'delete-job' => -> (namespace, args) { delete_job(namespace, args) },
     'exec' => -> (namespace, args) { exec(namespace, args) }
   }
 
@@ -25,28 +39,72 @@ class KubeClient
 
   class << self
     def deploy(namespace)
-      KubeCommands.apply(namespace, "web-deployment")
-      KubeCommands.apply(namespace, "worker-deployment")
+      if yes_no_prompt
+        KubeCommands.dockerize_app(PROJECT_PATH, DOCKER_HUB_UNAME, APP, VERSION)
+        KubeCommands.apply(namespace, "web-deployment")
+        KubeCommands.apply(namespace, "worker-deployment")
+      end
+    end
+
+    def delete(namespace, args)
+      if yes_no_prompt
+        KubeCommands.delete(namespace, args.first)
+      end
     end
 
     def migrate(namespace)
       KubeCommands.apply(namespace, "migrate")
     end
 
-    def scale(namespace)
+    def scale(namespace, args)
+      KubeCommands.scale(namespace, args)
+    end
 
+    def build_setup(namespace)
+      if yes_no_prompt
+        KubeCommands.dockerize_app(PROJECT_PATH, DOCKER_HUB_UNAME, APP, VERSION)
+        KubeCommands.setup(namespace)
+      end
     end
 
     def setup(namespace)
-      KubeCommands.setup(namespace)
+      if yes_no_prompt
+        KubeCommands.setup(namespace)
+      end
+    end
+
+    def add_job(namespace, args)
+      KubeCommands.apply(namespace, args.first)
+    end
+
+    def delete_job(namespace, args)
+      if yes_no_prompt
+        KubeCommands.delete(namespace, args.first)
+      end
     end
 
     def console(namespace)
-      KubeCommands.exec(namespace, "bin/rails c")
+      KubeCommands.exec(namespace, ["bin/rails", "c"])
     end
 
     def exec(namespace, args)
       KubeCommands.exec(namespace, args)
     end
+
+    private
+
+    def yes_no_prompt
+      loop do
+        print "Do you want to continue? (y/N): "
+        input = gets.chomp.downcase
+    
+        case input
+        when "y"
+          return true
+        else
+          puts false
+        end
+      end
+    end    
   end
 end
--- kube_commands.rb
+++ kube_commands.rb
@@ -1,17 +1,22 @@
 #!/usr/bin/env ruby
 
+require './kube_client.rb'
+
 class KubeCommands
+  FULL_CONFIG_PATH = "#{KubeClient::PROJECT_PATH}/#{KubeClient::CONFIG_PATH}"
+
   class << self
     def apply(namespace, manifest)
-      system("kubectl apply -n #{namespace} -f #{manifest}.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/#{manifest}.yml")
     end
 
-    def scale
-
+    def scale(namespace, args)
+      manifest, replicas = args
+      system("kubectl scale -n #{namespace} -f #{FULL_CONFIG_PATH}/#{manifest}.yml --replicas=#{replicas}")
     end
 
     def delete
-
+      system("kubectl delete -n #{namespace} -f #{FULL_CONFIG_PATH}/#{manifest}.yml")
     end
 
     def rollout
@@ -19,20 +24,29 @@ class KubeCommands
     end
 
     def setup(namespace)
-      system("kubectl apply -n #{namespace} -f postgres.yml")
-      system("kubectl apply -n #{namespace} -f postgres-service.yml")
-      system("kubectl apply -n #{namespace} -f redis-service.yml")
-      system("kubectl apply -n #{namespace} -f redis-service.yml")
-      system("kubectl apply -n #{namespace} -f web-deployment.yml")
-      system("kubectl apply -n #{namespace} -f worker-deployment.yml")
-      system("kubectl apply -n #{namespace} -f load-balancer.yml")
-      system("kubectl apply -n #{namespace} -f terminal.yml")
-      
-      ## schema load, migrate
+      system("kubectl create namespace #{namespace}")
+      system("kubectl create secret generic regcred --from-file=.dockerconfigjson=#{FULL_CONFIG_PATH}/secrets/docker-config.json --type=kubernetes.io/dockerconfigjson -n #{namespace}")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/secrets/rails-secrets.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/postgres.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/postgres.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/postgres-service.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/redis.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/redis-service.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/web-deployment.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/worker-deployment.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/load-balancer.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/terminal.yml")
+      system("kubectl apply -n #{namespace} -f #{FULL_CONFIG_PATH}/initializer.yml")
     end
 
-    def exec(namespace, command)
-      system("kubectl exec -it -n #{namespace} pod/terminal -- #{command}")
+    def exec(namespace, args)
+      system("kubectl exec -it -n #{namespace} pod/terminal -- #{args.join(" ")}")
+    end
+
+    def dockerize_app(project_path, docker_hub_uname, app, version)
+      system("cd #{project_path}")
+      system("docker buildx build --platform linux/amd64 -t #{docker_hub_uname}/#{app}:#{version} . --build-arg RAILS_MASTER_KEY=`cat config/credentials/production.key`")
+      system("docker push #{docker_hub_uname}/#{app}:#{version}")
     end
   end
 end
